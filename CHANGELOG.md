0ce05d9d6237d18b806faa4b00aabfd61efbdc0c
Museus <museus@protonmail.com>
2026-03-15 19:11:10 -0400

    build(deps): bump Hell2Modding dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Bump Hell2Modding dependency
+
 ## [0.0.1] - 2026-03-15
 
 ### Added
--- src/config.lua
+++ src/config.lua
@@ -1,5 +1,5 @@
 return {
-  version = "0.0.1",
+  version = "0.0.2",
   enabled = true,
   ModName = "SpeedrunTimer",
   trueUpInterval = 30, -- how often to call trueUp in frames. More frequent means more accurate but also more expensive.
