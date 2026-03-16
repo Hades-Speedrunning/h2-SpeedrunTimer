a816e75c8350ce4f3e872cea2c82fb48f3604c90
Museus <museus@protonmail.com>
2026-03-16 19:52:28 -0400

    update README, bump versions

--- src/config.lua
+++ src/config.lua
@@ -1,5 +1,5 @@
 return {
-  version = "0.0.2",
+  version = "0.0.3",
   enabled = true,
   ModName = "SpeedrunTimer",
   trueUpInterval = 30, -- how often to call trueUp in frames. More frequent means more accurate but also more expensive.
